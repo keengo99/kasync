4cca949b067d97b1131997330887e4a89485e9bc
keengo <3189199@qq.com>
2023-01-11 11:43:19 +0800

    add kgl_is_absolute_path

--- include/klib.h
+++ include/klib.h
@@ -3,5 +3,20 @@
 #include "kfeature.h"
 KBEGIN_DECLS
 void kgl_msleep(int msec);
+INLINE bool kgl_is_absolute_path(const char* str) {
+	if (*str == '/') {
+		return true;
+	}
+#ifdef _WIN32
+	if (*str == '\\') {
+		return true;
+	}
+	if (*str && *(str + 1) == ':') {
+		/* c:\dir */
+		return true;
+	}
+#endif
+	return false;
+}
 KEND_DECLS
 #endif
